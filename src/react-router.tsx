a107c02ff29166e7231280b2d9a8ded4183d025b
Rasmus Eneman <rasmus@eneman.eu>
2017-09-07 13:14:28 +0200

    Upgrade deps

--- src/react-router.tsx
+++ src/react-router.tsx
@@ -1,7 +1,10 @@
-import * as React from 'react'
+import PropTypes from 'prop-types'
+import React from 'react'
 import {Component} from 'react'
 import DefaultRoute from 'react-router/Route'
 import {HeroCompanion, heroTransitionContext} from './index'
+
+export {PropTypes as _PropTypes}
 
 export class HeroRoute extends Component<any, {}> {
   static contextTypes = heroTransitionContext
